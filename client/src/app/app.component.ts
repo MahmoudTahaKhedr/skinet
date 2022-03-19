1a24b19be74a8ce4ec30f0c24a5ce20d54ad6242
MahmoudTahaKhedr <92016207+MahmoudTahaKhedr@users.noreply.github.com>
2022-03-20 01:06:55 +0400

    End of Section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +10,13 @@ import { IProduct } from './models/product';
 })
 export class AppComponent  implements OnInit{
   title = 'Skinet';
-  products: IProduct[];
 
   
-  constructor(private http: HttpClient) {
+  constructor() {
     
     
   }
   ngOnInit(){
-    this.http.get('http://localhost:5000/api/products').subscribe(
-      (Response: IPagination) => {
-      this.products = Response.data;
-  },error =>{
-    console.log(error)
-      
-    });
+    
   }
 }
--- core/Specification/ProductsWithTypesAndBrandAndSpecification.cs
+++ core/Specification/ProductsWithTypesAndBrandAndSpecification.cs
@@ -25,7 +25,7 @@ namespace core.Specification
                         AddOrderBy(p => p.Price);
                         break;
 
-                    case "priceDsc":
+                    case "priceDesc":
                         AddOrderByDescending(p => p.Price);
                         break;
 
