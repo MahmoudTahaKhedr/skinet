a4c9c0b38f1feec71172156e1e9abb5a83a79549
MahmoudTahaKhedr <92016207+MahmoudTahaKhedr@users.noreply.github.com>
2022-03-27 23:23:02 +0400

    End of Section 12

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './shared/models/pagination';
-import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +14,6 @@ export class AppComponent  implements OnInit{
     
   }
   ngOnInit(){
-    
+   
   }
 }
